d138489c2d6e600f067340722073161a3ccfb363
Piotr <P.Larkowski90@gmail.com>
2024-03-26 16:07:24 +0100

    Add mechanic to Wizard class method isVunerable with connection to other method prepareSpell

--- src/Wizard.java
+++ src/Wizard.java
@@ -1,6 +1,21 @@
 public class Wizard extends Fighter{
+    boolean preparingSpell = false;
+    boolean vulnerable = true;
     @Override
     public String toString() {
         return "Fighter is a Wizard";
+    }
+
+    public void prepareSpell(){
+        preparingSpell = !preparingSpell;
+    }
+    @Override
+    boolean isVulnerable() {
+        if(preparingSpell){
+            vulnerable = false;
+        }else{
+            vulnerable = true;
+        }
+        return vulnerable;
     }
 }
