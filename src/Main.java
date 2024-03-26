b347b6caed8d5a9c6466e8a33ee009d29a682155
Piotr <P.Larkowski90@gmail.com>
2024-03-26 16:15:03 +0100

    Add uses to main clas of Warrior and Wizard class - example battle

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Warrior warrior = new Warrior();
         Wizard wizard = new Wizard();
+        System.out.print("Warrior attack wizard - ");
         System.out.println(warrior.getDamagePoints(wizard));
+        System.out.print("Wizard attack warrior - ");
+        System.out.println(wizard.getDamagePoints(warrior));
+        System.out.print("Warrior attack wizard - ");
+        System.out.println(warrior.getDamagePoints(wizard));
+        System.out.println("Wizard prepare spell");
+        wizard.prepareSpell();
+        System.out.print("Wizard attack warrior - ");
+        System.out.println(wizard.getDamagePoints(warrior));
     }
 }
